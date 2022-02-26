fbaa09e741b5d64c2de5e0cea7cd06150210c394
Ludovic Eschard <ludovic@eschard.com>
2022-02-26 18:03:21 +0100

    feat(pointvy): define pointvy version

--- app/main.py
+++ app/main.py
@@ -9,12 +9,14 @@ from flask import redirect
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def landing():
     content = ""
     action = url_for("trivy_scan")
     version = os.environ.get('TRIVY_VERSION')
-    return render_template('main.html', content=content, action=action, version=version)
+    return render_template('main.html', content=content,
+                           action=action, version=version)
 
 
 @app.route("/scan/")
@@ -27,9 +29,10 @@ def trivy_scan():
         bash_escape = re.compile(r'[^a-zA-Z0-9\:\-\.\ \,\/]')
         query_sanitized = bash_escape.sub('', query)
         cmd = "./trivy image " + format(query_sanitized)
-        content = os.popen(cmd).read()  # nosec - user input is sanitized before
+        content = os.popen(cmd).read()  # nosec - user input is sanitized
 
-        version = os.environ.get('TRIVY_VERSION')
+        trivy_version = os.environ.get('TRIVY_VERSION')
+        pointvy_version = os.environ.get('POINTVY_VERSION')
 
         # remove colors special characters
         ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
@@ -40,10 +43,11 @@ def trivy_scan():
 
     action = url_for("trivy_scan")
     return render_template("main.html", content=result,
-                        action=action, query=query_sanitized,
-                        version=version)
+                           action=action, query=query_sanitized,
+                           trivy_version=trivy_version,
+                           pointvy_version=pointvy_version)
 
 
 if __name__ == "__main__":
 
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))  # nosec - we are inside a container
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))  # nosec
