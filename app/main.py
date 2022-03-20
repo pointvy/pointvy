ea5c1ff47e542ce2c43d4951d19f7bcfda0c3000
Ludovic Eschard <ludovic@eschard.com>
2022-03-20 21:37:40 +0100

    feat: add no-progress option when lauching Trivy

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ def trivy_scan():
         # delete every char except a-z A-Z 0-9 : - . , / and space
         bash_escape = re.compile(r'[^a-zA-Z0-9\:\-\.\ \,\/]')
         query_sanitized = bash_escape.sub('', query)
-        cmd = "./trivy image " + format(query_sanitized)
+        cmd = "./trivy image --no-progress " + format(query_sanitized)
         content = os.popen(cmd).read()  # nosec - user input is sanitized
 
         trivy_version = os.environ.get('TRIVY_VERSION')
