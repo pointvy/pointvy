738d5bdc5b27f787171b66b1c8112cad1c3d0cf6
Ludovic Eschard <ludovic@eschard.com>
2022-10-15 17:36:12 +0200

    fix: ignore semgrep on app binding (containerized app)

--- app/main.py
+++ app/main.py
@@ -52,4 +52,4 @@ def trivy_scan():
 
 if __name__ == "__main__":
 
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))  # nosec
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))  # nosec // nosemgrep
