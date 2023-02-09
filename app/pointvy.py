b448efcc4e91f9b257eb805920ccdbabdafc5501
Ludovic Eschard <ludovic@eschard.com>
2023-02-09 20:49:35 +0100

    Refactoring (#70)
    * chore: bump to Python 3.11.1 and Alpine 3.17
    
    * chore: factor code
    
    * chore: rename main file
    
    * fix: better writing
    
    * chore: format

--- app/pointvy.py
+++ app/pointvy.py
@@ -9,32 +9,29 @@ from flask import redirect
 
 app = Flask(__name__)
 
+TRIVY_VERSION = os.environ.get("TRIVY_VERSION")
+POINTVY_VERSION = os.environ.get("POINTVY_VERSION")
+
 
 @app.route("/")
 def landing():
-    content = ""
     action = url_for("trivy_scan")
-    trivy_version = os.environ.get('TRIVY_VERSION')
-    pointvy_version = os.environ.get('POINTVY_VERSION')
-    return render_template('main.html', content=content,
-                           action=action, trivy_version=trivy_version,
-                           pointvy_version=pointvy_version)
+    return render_template('main.html', content="",
+                           action=action, trivy_version=TRIVY_VERSION,
+                           pointvy_version=POINTVY_VERSION)
 
 
 @app.route("/scan/")
 def trivy_scan():
     query = request.args.get("q")
 
-    if query and query != "":
+    if query:
 
         # delete every char except a-z A-Z 0-9 : - . , / and space
         bash_escape = re.compile(r'[^a-zA-Z0-9\:\-\.\ \,\/]')
         query_sanitized = bash_escape.sub('', query)
         cmd = "./trivy image --no-progress " + format(query_sanitized)
         content = os.popen(cmd).read()  # nosec - user input is sanitized
-
-        trivy_version = os.environ.get('TRIVY_VERSION')
-        pointvy_version = os.environ.get('POINTVY_VERSION')
 
         # remove colors special characters
         ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
@@ -46,10 +43,10 @@ def trivy_scan():
     action = url_for("trivy_scan")
     return render_template("main.html", content=result,
                            action=action, query=query_sanitized,
-                           trivy_version=trivy_version,
-                           pointvy_version=pointvy_version)
+                           trivy_version=TRIVY_VERSION,
+                           pointvy_version=POINTVY_VERSION)
 
 
 if __name__ == "__main__":
 
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))  # nosec // nosemgrep
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080))) # nosec // nosemgrep
