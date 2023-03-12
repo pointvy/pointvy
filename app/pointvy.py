dad051a0f61e8174e8da2892a40e2f9baf887a6c
Ludovic Eschard <ludovic@eschard.com>
2023-03-12 16:50:32 +0100

    feat: handle error with subprocess

--- app/pointvy.py
+++ app/pointvy.py
@@ -1,5 +1,6 @@
 import os
 import re
+import subprocess
 
 from flask import Flask
 from flask import request
@@ -37,15 +38,39 @@ def trivy_scan():
         # delete every char except a-z A-Z 0-9 : - . , / and space
         bash_escape = re.compile(r'[^a-zA-Z0-9\:\-\.\ \,\/]')
         query_sanitized = bash_escape.sub('', query)
-        cmd = "./trivy image --scanners vuln --no-progress "
+
+        cmd = ["./trivy", "image", "--scanners", "vuln", "--no-progress"]
 
         checked_value = ""
         if request.args.get("ignore-unfixed") == "true":
-            cmd += "--ignore-unfixed "
+            cmd.append("--ignore-unfixed")
             checked_value = "checked"
 
-        cmd += format(query_sanitized)
-        content = os.popen(cmd).read()  # nosec - user input is sanitized
+        cmd.append(format(query_sanitized))
+
+        error = ""
+        content = ""
+
+        ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+
+        try:
+            res = subprocess.Popen(
+                cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output, error_handler = res.communicate()
+            if output:
+                # print(f"OK> output {output}")
+                content = output.decode('utf-8')
+            else:
+                if error_handler:
+                    # print(f"Error> error {error_handler.strip()}")
+                    error = error_handler.strip().decode('utf-8')
+                    error = ansi_escape.sub('', error)
+        except OSError as e:
+            print(f"OSError > {e.errno}")
+            print(f"OSError > {e.strerror}")
+            print(f"OSError > {e.filename}")
+
+            error = e.strerror.decode('utf-8')
 
         # remove colors special characters
         ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
@@ -59,7 +84,7 @@ def trivy_scan():
                            action=action, query=query_sanitized,
                            trivy_version=get_trivy_version(),
                            pointvy_version=POINTVY_VERSION,
-                           checked=checked_value)
+                           checked=checked_value, error=error)
 
 
 if __name__ == "__main__":
